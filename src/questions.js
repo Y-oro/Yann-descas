694203a0c2bfe7ea2a6bfae6a0d4b55c0c7a437f
Yann Deska <yann.dsk@icloud.com>
2025-05-26 22:05:10 +0200

    questionnaire NBA

---
+++ src/questions.js
@@ -0,0 +1,92 @@
+const questions = [
+  {
+    qid: 1,
+    qlabel: "Combien de joueurs par équipe sont sur le terrain en même temps ?",
+    reponses: [
+      { rid: 1, rlabel: "5" }, // vrai
+      { rid: 2, rlabel: "6" },
+      { rid: 3, rlabel: "7" }
+    ]
+  },
+  {
+    qid: 2,
+    qlabel: "Quelle équipe a remporté le plus de titres NBA ?",
+    reponses: [
+      { rid: 1, rlabel: "Los Angeles Lakers" },
+      { rid: 2, rlabel: "Boston Celtics" }, // vrai
+      { rid: 3, rlabel: "Chicago Bulls" }
+    ]
+  },
+  {
+    qid: 3,
+    qlabel: "Quel joueur est surnommé 'The King' ?",
+    reponses: [
+      { rid: 1, rlabel: "LeBron James" }, // vrai
+      { rid: 2, rlabel: "Kevin Durant" },
+      { rid: 3, rlabel: "Stephen Curry" }
+    ]
+  },
+  {
+    qid: 4,
+    qlabel: "Combien de points vaut un tir derrière la ligne à 3 points ?",
+    reponses: [
+      { rid: 1, rlabel: "2" },
+      { rid: 2, rlabel: "3" },// vrai
+      { rid: 3, rlabel: "4" }
+    ]
+  },
+  {
+    qid: 5,
+    qlabel: "Quel joueur a remporté 6 titres NBA avec les Bulls ?",
+    reponses: [
+      { rid: 1, rlabel: "Michael Jordan" }, // vrai
+      { rid: 2, rlabel: "Kobe Bryant" },
+      { rid: 3, rlabel: "Shaquille O'Neal" }
+    ]
+  },
+  {
+    qid: 6,
+    qlabel: "Combien de fautes personnelles avant une exclusion en NBA ?",
+    reponses: [
+      { rid: 1, rlabel: "5" },
+      { rid: 2, rlabel: "6" },// vrai
+      { rid: 3, rlabel: "7" }
+    ]
+  },
+  {
+    qid: 7,
+    qlabel: "Quel est le surnom de Victor Wembanyama?",
+    reponses: [
+      { rid: 1, rlabel: "The giant" },
+      { rid: 2, rlabel: "Wemby" }, // vrai
+      { rid: 3, rlabel: "Frenchi" }
+    ]
+  },
+  {
+    qid: 8,
+    qlabel: "Quel est le nom du championnat nord-américain de basket ?",
+    reponses: [
+      { rid: 1, rlabel: "NFL" },
+      { rid: 2, rlabel: "NBA" },// vrai
+      { rid: 3, rlabel: "NHL" }
+    ]
+  },
+  {
+    qid: 9,
+    qlabel: "Combien de temps dure un quart-temps en NBA ?",
+    reponses: [
+      { rid: 1, rlabel: "10 minutes" },
+      { rid: 2, rlabel: "12 minutes" },// vrai
+      { rid: 3, rlabel: "15 minutes" }
+    ]
+  },
+  {
+    qid: 10,
+    qlabel: "Quel joueur est célèbre pour ses tirs à 3 points avec les Warriors ?",
+    reponses: [
+      { rid: 1, rlabel: "Stephen Curry" }, // vrai
+      { rid: 2, rlabel: "Klay Thompson" },
+      { rid: 3, rlabel: "James Harden" }
+    ]
+  }
+];
